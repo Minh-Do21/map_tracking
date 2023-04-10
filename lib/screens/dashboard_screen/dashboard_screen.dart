e4d95ca9f3aaef31905aedfeef60be42fdc56985
dotm@mor.com.vn <dotm@mor.com.vn>
2023-04-10 13:56:27 +0700

    test error

--- lib/screens/dashboard_screen/dashboard_screen.dart
+++ lib/screens/dashboard_screen/dashboard_screen.dart
@@ -66,18 +66,14 @@ class _DashboardScreenFormState extends State<_DashboardScreenForm> {
     );
 
     if (result.points.isNotEmpty) {
-                    print("Được nhéd");
-                    print("Được nhéd");
+      print("Được nhéd");
+      print("Được nhéd");
       for (var element in result.points) {
-        polylineCoordinates.
-        add(            
-          
-          
-           LatLng(element.latitude, element.longitude));
+        polylineCoordinates.add(LatLng(element.latitude, element.longitude));
       }
       setState(() {});
     } else {
-                          print("Không nhéd");
+      print("Không nhéd");
     }
   }
 
